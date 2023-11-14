a873aa8afabead64c117ba13977e10c213c7003b
gagan-chauhan <gagan2020chauhan@gmail.com>
2023-11-14 20:27:26 +0530

    operations -> find, updateOne, deleteOne

--- server.js
+++ server.js
@@ -29,13 +29,67 @@ const startServer = async () => {
             res.json(family);
         });
 
+        // *******************************************************************
+        // Create new user in database :)
+
         const user1 = new UserData({
-            email: "gagan123@gmail.com",
-            password: "password"
+            email: "abc123@gmail.com",
+            password: "helloabc2003"
         });
 
-        await user1.save();
-        console.log("User data is inserted");
+        // await user1.save();
+        // console.log("User data is inserted");
+
+        // *******************************************************************
+
+        // *******************************************************************
+        // find users with given credential in database :)
+
+        // await UserData.find({email: "abc123@gmail.com"})
+        //     .then((users)=>{
+        //         console.log("Users with this credential in database are: ", users);
+        //     })
+        //     .catch((err)=>{
+        //         console.error(err);
+        //     })
+
+        // find all users in database :)
+
+        // await UserData.find()
+        //     .then((users)=>{
+        //         console.log("All users in database are: ", users);
+        //     })
+        //     .catch((err)=>{
+        //         console.error(err);
+        //     })
+
+        // *******************************************************************
+
+        // *******************************************************************
+        // updateOne user in database :)
+
+        // await UserData.updateOne({email: "gagan123@gmail.com"}, {password: "hellogagan"})
+        //     .then(()=>{
+        //         console.log("password updated");
+        //     })
+        //     .catch((err)=>{
+        //         console.error(err);
+        //     })
+
+        // *******************************************************************
+
+        // *******************************************************************
+        // deleteOne user in database :)
+
+        // await UserData.deleteOne({email: "gagan123@gmail.com"})
+        //     .then(()=>{
+        //         console.log("user deleted");
+        //     })
+        //     .catch((err)=>{
+        //         console.error(err);
+        //     })
+
+        // *******************************************************************
 
         app.listen(PORT, () => {
             console.log(`Server is started on PORT: ${PORT}`);
